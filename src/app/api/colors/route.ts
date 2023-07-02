edd78787349e6f6da758406abfc6b50a82006bbc
Edward <edward821220@gmail.com>
2023-07-02 12:50:51 +0800

    project init

---
+++ src/app/api/colors/route.ts
@@ -0,0 +1,28 @@
+import { NextResponse } from "next/server";
+import { Configuration, OpenAIApi } from "openai";
+
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+const openai = new OpenAIApi(configuration);
+
+export async function POST(request: Request) {
+  const req = await request.json();
+  const chatCompletion = await openai.createChatCompletion({
+    model: "gpt-3.5-turbo",
+    messages: [
+      {
+        role: "system",
+        content: "",
+      },
+      {
+        role: "user",
+        content: `${req.message}`,
+      },
+    ],
+  });
+  const data = chatCompletion.data.choices[0].message;
+  console.log(data);
+
+  return NextResponse.json({ data });
+}
